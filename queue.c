960b4ba52f4920c4a0e4537170b12011305c276c
dainelsour <dainel4356899@gmail.com>
2025-03-20 01:52:39 +0800

    Implement queue insertion functions
    Add functions for inserting elements at both ends of the queue.
    Handle proper memory allocation and edge cases like NULL queues.
    
    Change-Id: I0000000000000000000000000000000000000000

--- queue.c
+++ queue.c
@@ -33,12 +33,40 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *new_element = (element_t *) malloc(sizeof(element_t));
+    if (!new_element)
+        return false;
+
+    new_element->value = strdup(s);
+    if (!new_element->value) {
+        free(new_element);
+        return false;
+    }
+
+    list_add(&new_element->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *new_element = (element_t *) malloc(sizeof(element_t));
+    if (!new_element)
+        return false;
+
+    new_element->value = strdup(s);
+    if (!new_element->value) {
+        free(new_element);
+        return false;
+    }
+
+    list_add_tail(&new_element->list, head);
     return true;
 }
 
